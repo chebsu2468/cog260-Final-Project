f8f200b6b1e427a1b4a2393350c785b0389934ef
jasmine-artsci <jasmine.jasmine@mail.utoronto.ca>
2026-03-28 11:37:40 -0400

    revert back to using pyClarion-lib

--- pyClarion/pyClarion/events/__init__.py
+++
@@ -1,8 +0,0 @@
-from .system import Update, Event, Clock, Process
-from .sites import State, Site
-from .updates import StateUpdate, ForwardUpdate, BackwardUpdate, KeyspaceUpdate
-
-__all__ = [
-    "Update", "Event", "Clock", "Process", "State", "Site", "StateUpdate", 
-    "ForwardUpdate", "BackwardUpdate", "KeyspaceUpdate"
-]
